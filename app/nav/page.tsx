56ec41e008719382ccb63734159e83f1c27c4b92
sanuj <sanujkumar67943@gmail.com>
2025-04-19 00:02:45 +0530

    add margin padding in skeleton

--- app/nav/page.tsx
+++ app/nav/page.tsx
@@ -4,49 +4,49 @@ export default function SkeletonCard() {
     return (
         <div className=" ml-10  grid-col-12">
             <div className=" grid grid-cols-2 md:grid-cols-3 ">  
-            <div className="flex flex-col space-y-3 ">
+            <div className="flex flex-col space-y-3 pb-4">
                 <Skeleton className="h-[125px] w-[250px] rounded-xl" />
                 <div className="space-y-2">
                     <Skeleton className="h-4 w-[250px]" />
                     <Skeleton className="h-4 w-[200px]" />
                 </div>  
             </div>
-            <div className="flex flex-col space-y-3">
+            <div className="flex flex-col space-y-3 pb-4">
                 <Skeleton className="h-[125px] w-[250px] rounded-xl" />
                 <div className="space-y-2">
                     <Skeleton className="h-4 w-[250px]" />
                     <Skeleton className="h-4 w-[200px]" />
                 </div>
             </div>
-            <div className="flex flex-col space-y-3">
+            <div className="flex flex-col space-y-3 pb-4">
                 <Skeleton className="h-[125px] w-[250px] rounded-xl" />
                 <div className="space-y-2">
                     <Skeleton className="h-4 w-[250px]" />
                     <Skeleton className="h-4 w-[200px]" />
                 </div>
             </div>
-            <div className="flex flex-col space-y-3">
+            <div className="flex flex-col space-y-3 pb-4">
                 <Skeleton className="h-[125px] w-[250px] rounded-xl" />
                 <div className="space-y-2">
                     <Skeleton className="h-4 w-[250px]" />
                     <Skeleton className="h-4 w-[200px]" />
                 </div>
             </div>
-            <div className="flex flex-col space-y-3">
+            <div className="flex flex-col space-y-3pb-4">
                 <Skeleton className="h-[125px] w-[250px] rounded-xl" />
                 <div className="space-y-2">
                     <Skeleton className="h-4 w-[250px]" />
                     <Skeleton className="h-4 w-[200px]" />
                 </div>
             </div>
-            <div className="flex flex-col space-y-3">
+            <div className="flex flex-col space-y-3 pb-4">
                 <Skeleton className="h-[125px] w-[250px] rounded-xl" />
                 <div className="space-y-2">
                     <Skeleton className="h-4 w-[250px]" />
                     <Skeleton className="h-4 w-[200px]" />
                 </div>
             </div>
-            <div className="flex flex-col space-y-3">
+            <div className="flex flex-col space-y-3 pb-4">  
                 <Skeleton className="h-[125px] w-[250px] rounded-xl" />
                 <div className="space-y-2">
                     <Skeleton className="h-4 w-[250px]" />
